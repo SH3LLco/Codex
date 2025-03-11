2fc34b9371ed84734f97fc09d8f453d762926921
SH3LL <root@sh3ll.co>
2025-03-11 18:27:15 -0500

    move stuff

--- docs/javascripts/load.js
+++ docs/javascripts/load.js
@@ -1,6 +1,4 @@
 document$.subscribe(function() {
-  console.log("Re-initializing third-party plugins...");
-  
   var tables = document.querySelectorAll("article table:not([class])")
   tables.forEach(function(table) {
     new Tablesort(table)
